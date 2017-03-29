3d4107ef8aa5f8c0656e121d240986a36c91d1b1
“Ivan <nohemi.baol@gmail.com>
2017-03-29 13:13:01 -0600

    segunda versión

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
 function agregarPendiente(){
-    var texto=document.getElementById('liname').value;
-    var li=document.createElement('LI');
+    var texto=document.getElementById("linombre").value;
+    var li=document.createElement("LI");
     li.innerHTML=texto==''?'(nada)':texto;
-    document.getElementById('lista').appendChild(li);
+    document.getElementById("lista").appendChild(li);
 }
 
-function clone(){
-    function crearLista(){
-    var nuLista = document.getElementById("lista");
+
+function crearLista(){
+    var nuLista = document.getElementById("listaPen");
     var cln = nuLista.cloneNode(true);
     document.getElementById("nu").appendChild(cln);
-}
-}
 
+
+}
+/*la lista se clona, pero al presionar sobre sus elementos estos no se borran :(
+Creo que podría usar un objeto, pero no estoy segura, así que lo dejo así*/
 
 
 function borrarElemento(){
-    lis=document.getElementById('lista').getElementsByTagName('li');
+    lis=document.getElementById("lista").getElementsByTagName("li");
     for(var i=0; i<lis.length;i++){
         lis[i].onclick=function(){
-            if(confirm('¿Borrar este pendiente?'))
+            if(confirm("¿Eliminar este pendiente?"))
             this.parentNode.removeChild(this);
         };
     }
-    
+};
+
+
+/*function borrarLista(){
+    var seccion = document.getElementsByTagName("section");
+    if(seccion.hasChildNodes()){
+        seccion.removeChild(seccion.childNodes[1]);
+    }
+}*/
+
+function borrarLista(){
+    var eliminar = document.getElementById("listaPen");
+    eliminar.onclick=function(){
+    if(confirm("¿Eliminar esta lista?")){
+        this.removeChild(this);
+        }
+    }
 }
+
+//Ya no sé qué estoy haciendo...
